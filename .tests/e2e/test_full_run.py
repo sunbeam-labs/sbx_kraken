8a0f533957097d339b91964278ce3d26e93b4332
ctbus <ctbushman@gmail.com>
2022-12-12 17:14:05 -0500

    Reformatting and pip install wget

--- .tests/e2e/test_full_run.py
+++ .tests/e2e/test_full_run.py
@@ -6,12 +6,15 @@ import tempfile
 import unittest
 import wget
 
+
 class FullRunTests(unittest.TestCase):
     def setUp(self) -> None:
         self.temp_dir = tempfile.mkdtemp()
 
         self.db_fp = os.path.join(self.temp_dir, "viral-db/")
-        viral_db_url = "https://genome-idx.s3.amazonaws.com/kraken/k2_viral_20220908.tar.gz"
+        viral_db_url = (
+            "https://genome-idx.s3.amazonaws.com/kraken/k2_viral_20220908.tar.gz"
+        )
         wget.download(viral_db_url, out=self.db_fp)
         tar = tarfile.open(self.db_fp, "r:gz")
         tar.extractall(path=self.db_fp)
@@ -21,44 +24,47 @@ class FullRunTests(unittest.TestCase):
 
         self.project_dir = os.path.join(self.temp_dir, "project/")
 
-        sp.check_output([
-            "sunbeam",
-            "init",
-            "--data_fp",
-            self.reads_fp,
-            self.project_dir
-        ])
+        sp.check_output(
+            ["sunbeam", "init", "--data_fp", self.reads_fp, self.project_dir]
+        )
 
         self.config_fp = os.path.join(self.project_dir, "sunbeam_config.yml")
 
-        sp.check_output([
-            "sunbeam",
-            "config",
-            "modify",
-            "-i",
-            "-f", f"{self.config_fp}"
-            "-s", f"'sbx_kraken: {{kraken_db_fp: {self.db_fp}}}'"
-        ])
+        sp.check_output(
+            [
+                "sunbeam",
+                "config",
+                "modify",
+                "-i",
+                "-f",
+                f"{self.config_fp}" "-s",
+                f"'sbx_kraken: {{kraken_db_fp: {self.db_fp}}}'",
+            ]
+        )
 
         self.output_fp = os.path.join(self.project_dir, "sunbeam_output")
-        #shutil.copytree(".tests/data/sunbeam_output", self.output_fp)
+        # shutil.copytree(".tests/data/sunbeam_output", self.output_fp)
 
-        self.all_samples_fp = os.path.join(self.output_fp, "classify/kraken/all_samples.tsv")
-    
+        self.all_samples_fp = os.path.join(
+            self.output_fp, "classify/kraken/all_samples.tsv"
+        )
+
     def tearDown(self):
         shutil.rmtree(self.temp_dir)
-    
+
     def test_full_run(self):
         # Run the test job.
-        sp.check_output([
-            "sunbeam",
-            "run",
-             "--profile", 
-            self.project_dir,
-            "all_classify",
-            "--directory",
-            self.temp_dir,
-        ])
+        sp.check_output(
+            [
+                "sunbeam",
+                "run",
+                "--profile",
+                self.project_dir,
+                "all_classify",
+                "--directory",
+                self.temp_dir,
+            ]
+        )
 
         # Check output
         self.assertTrue(os.path.exists(self.all_samples_fp))
