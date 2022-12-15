d98f7bc4c1fda5524f286f57fa25728f0ef838c9
ctbus <ctbushman@gmail.com>
2022-12-14 22:25:01 -0500

    ReformatA

--- .tests/e2e/test_full_run.py
+++ .tests/e2e/test_full_run.py
@@ -15,9 +15,7 @@ def setup():
 
     project_dir = os.path.join(temp_dir, "project/")
 
-    sp.check_output(
-        ["sunbeam", "init", "--data_fp", reads_fp, project_dir]
-    )
+    sp.check_output(["sunbeam", "init", "--data_fp", reads_fp, project_dir])
 
     config_fp = os.path.join(project_dir, "sunbeam_config.yml")
 
@@ -39,6 +37,7 @@ def setup():
 
     shutil.rmtree(temp_dir)
 
+
 @pytest.fixture
 def run_sunbeam(setup):
     temp_dir, project_dir = setup
@@ -58,19 +57,19 @@ def run_sunbeam(setup):
 
     output_fp = os.path.join(project_dir, "sunbeam_output")
 
-    all_samples_fp = os.path.join(
-        output_fp, "classify/kraken/all_samples.tsv"
-    )
+    all_samples_fp = os.path.join(output_fp, "classify/kraken/all_samples.tsv")
 
     benchmarks_fp = os.path.join(project_dir, "stats/")
 
     yield all_samples_fp, benchmarks_fp
+
 
 def test_full_run(run_sunbeam):
     all_samples_fp, benchmarks_fp = run_sunbeam
 
     # Check output
     assert os.path.exists(all_samples_fp)
+
 
 def test_benchmarks(run_sunbeam):
     all_samples_fp, benchmarks_fp = run_sunbeam
