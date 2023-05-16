f8a36ba2f30d62e821a4b7051f68f6f079ba505a
ctbus <ctbushman@gmail.com>
2023-05-16 12:20:03 -0400

    Fliperoo

--- scripts/kraken2_classify_report.py
+++ scripts/kraken2_classify_report.py
@@ -1,31 +1,35 @@
 import gzip
 import subprocess as sp
 
-def is_empty():
+
+def not_empty():
     with gzip.open(snakemake.input[0], "rb") as f:
         try:
             file_content = f.read(1)
             return len(file_content) > 0
         except:
             return False
-        
-if is_empty():
-    with open(snakemake.log, "w") as f_log, open(snakemake.output.report, "w") as f_out:
-        f_log.write("Empty reads files")
-        f_out.write("0\t0.0\tk__Bacteria; p__; c__; o__; f__; g__; s__")
-else:
-    output = sp.check_output([
-        "kraken2",
-        "--gzip-compressed",
-        "--db",
-        f"{snakemake.params.db}",
-        "--report",
-        f"{snakemake.output.report}",
-        "--output",
-        f"{snakemake.output.raw}",
-        f"{snakemake.params.paired_end}",
-        f"{snakemake.input}",
-    ])
+
+
+if not_empty():
+    output = sp.check_output(
+        [
+            "kraken2",
+            "--gzip-compressed",
+            "--db",
+            f"{snakemake.params.db}",
+            "--report",
+            f"{snakemake.output.report}",
+            "--output",
+            f"{snakemake.output.raw}",
+            f"{snakemake.params.paired_end}",
+            f"{snakemake.input}",
+        ]
+    )
 
     with open(snakemake.log, "wb") as f:
         f.write(output)
+else:
+    with open(snakemake.log, "w") as f_log, open(snakemake.output.report, "w") as f_out:
+        f_log.write("Empty reads files")
+        f_out.write("0\t0.0\tk__Bacteria; p__; c__; o__; f__; g__; s__")
