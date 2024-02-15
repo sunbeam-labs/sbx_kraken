fb8f1aa9b14b3e98dc75382c410076f2592c4b03
Ulthran <ctbushman@gmail.com>
2024-02-15 10:07:46 -0500

    Update test checks

--- .tests/e2e/test_full_run.py
+++ .tests/e2e/test_full_run.py
@@ -46,14 +46,12 @@ def setup(tmpdir):
 
     yield tmpdir, project_dir
 
-    shutil.rmtree(tmpdir)
-
 
 @pytest.fixture
 def run_sunbeam(setup):
     tmpdir, project_dir = setup
 
-    output_fp = os.path.join(project_dir, "sunbeam_output")
+    output_fp = project_dir / "sunbeam_output"
 
     try:
         # Run the test job
@@ -71,14 +69,14 @@ def run_sunbeam(setup):
             ]
         )
     except sp.CalledProcessError as e:
-        shutil.copytree(os.path.join(output_fp, "logs/"), "logs/")
-        shutil.copytree(os.path.join(project_dir, "stats/"), "stats/")
+        shutil.copytree(output_fp / "logs", "logs/")
+        shutil.copytree(project_dir / "stats", "stats/")
         sys.exit(e)
 
-    shutil.copytree(os.path.join(output_fp, "logs/"), "logs/")
-    shutil.copytree(os.path.join(project_dir, "stats/"), "stats/")
+    shutil.copytree(output_fp / "logs", "logs/")
+    shutil.copytree(project_dir / "stats", "stats/")
 
-    benchmarks_fp = os.path.join(project_dir, "stats/")
+    benchmarks_fp = project_dir / "stats"
 
     yield output_fp, benchmarks_fp
 
@@ -86,24 +84,16 @@ def run_sunbeam(setup):
 def test_full_run(run_sunbeam):
     output_fp, benchmarks_fp = run_sunbeam
 
-    all_samples_fp = os.path.join(output_fp, "classify/kraken/all_samples.tsv")
+    all_samples_fp = output_fp / "classify" / "kraken" / "all_samples.tsv"
 
     # Check output
-    assert os.path.exists(all_samples_fp)
+    assert all_samples_fp.exists()
 
     with open(all_samples_fp) as f:
-        f.readline()
-        f.readline()  # Headers
-        lines = f.readlines()
-        print(lines)
-        assert any(
-            [
-                "2\t0.0\t200.0\tk__Bacteria; p__; c__; o__; f__; g__; s__" in x.strip()
-                for x in lines
-            ]
-        )
+        header_line = f.readline()
+        assert "TEST-taxa" in header_line
+        assert "EMPTY-taxa" in header_line
 
-    with open(os.path.join(output_fp, "classify/kraken/EMPTY-taxa.tsv")) as f:
-        assert (
-            f.readline().strip() == "0\t0.0\tk__Bacteria; p__; c__; o__; f__; g__; s__"
-        )
+        bacteria_line = f.readline()
+        print(bacteria_line)
+        assert "2\t394.0\t0.0" in bacteria_line
